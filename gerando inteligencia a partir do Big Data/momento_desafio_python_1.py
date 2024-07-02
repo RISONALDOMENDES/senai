95b9c637b91eea7c768454086c93a227af2ce50a
RISONALDOMENDES <102391306+RISONALDOMENDES@users.noreply.github.com>
2024-07-02 13:15:08 -0300

    curso senai - big data

---
+++ gerando inteligencia a partir do Big Data/momento_desafio_python_1.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Resposta ao Momento Desafio 1 de programação em Python.
+
+Lembre-se: não existe uma única resposta para os desafios.
+Considere o resultado do código abaixo e veja se ele se aproxima do que você conseguiu com o seu.
+
+"""
+
+"Criando duas varíaveis A (String) e B(Inteiro)"
+A = "10.5"
+B = 2
+
+"Calculando A + B"
+C = float(A) + B
+print(C)
+
+"Calculando A**B"
+D = float(A)**B
+print(D)
+
+"Calculando A%B"
+E = float(A) % B
+print(E)
