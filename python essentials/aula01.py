d91bfdc26fc62600a532f8a6a2386d0cda4bcc44
RISONALDOMENDES <102391306+RISONALDOMENDES@users.noreply.github.com>
2024-07-03 18:59:47 -0300

    funcao print

---
+++ python essentials/aula01.py
@@ -0,0 +1,6 @@
+print("Hello, Word!!")
+print("hello,","Word!!", sep=" " )
+print("hello,","Word!!", sep="_" )
+print()
+print("hello,","Word!!", sep=" ", end=" ")
+print("hello,","Word!!", sep="**" )
